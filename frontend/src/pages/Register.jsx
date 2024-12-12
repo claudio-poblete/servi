0ec15279efbb86e0dc44d1256a1ab7dd9d3d7d44
Julio Olivares <julio@por-fin.cl>
2024-12-11 23:27:47 -0300

    ..

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import AuthContextModule from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
-import ButtonLink from "../components/buttons/ButtonLink";
 
 const Register = () => {
   const { register } = AuthContextModule.useAuth();
